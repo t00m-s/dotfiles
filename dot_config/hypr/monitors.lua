42db2c1846f032982f86c91f0cf1699897d1a3c4
Tommaso Soncin <soncintommaso@gmail.com>
2026-05-18 14:27:58 +0200

    feat: adding hyprland to chezmoi repo

---
+++ dot_config/hypr/monitors.lua
@@ -0,0 +1,18 @@
+hl.monitor {
+  output = 'HDMI-A-1',
+  mode = 'preferred',
+  position = 'auto',
+  scale = 'auto',
+}
+hl.monitor {
+  output = 'eDP-1',
+  mode = '1920x1200@144',
+  position = '0x0',
+  scale = 'auto',
+}
+hl.monitor {
+  output = 'DP-1',
+  mode = 'highres',
+  position = 'auto',
+  scale = 'auto',
+}
