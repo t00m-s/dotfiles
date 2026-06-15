fee57e52a30a27bf500b81210daf9f16ee1e625e
Tommaso Soncin <soncintommaso@gmail.com>
2026-06-15 11:37:51 +0200

    attempt to make screen sharing work, with no success

--- dot_config/nixos/modules/niri.nix
+++ dot_config/nixos/modules/niri.nix
@@ -8,7 +8,10 @@
   # services.displayManager.gdm = {
   #   enable = true;
   # };
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+  };
   security.polkit.enable = true; # polkit
   services.gnome.gnome-keyring.enable = true; # secret service
   services.displayManager.sddm = {
@@ -26,7 +29,17 @@
     inputs.noctalia.packages.${stdenv.hostPlatform.system}.default
     playerctl
     brightnessctl
+    hypridle
   ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk # Added for fallback UI elements
+      xdg-desktop-portal-wlr
+    ];
+    config.common.default = [ "gnome" "gtk" "wlr" ];
+  };
 
   # 4. Fonts and Emojis Setup
   fonts.packages = with pkgs; [
@@ -34,15 +47,22 @@
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
 
+    # Base Fonts (CRITICAL: Firefox needs these if they are set as defaults)
+    noto-fonts
+    
     # Emoji Support
     noto-fonts-color-emoji
     twemoji-color-font
   ];
 
   # Direct system to look at emojis properly
-  fonts.fontconfig.defaultFonts = {
-    monospace = [ "FiraCode Nerd Font" "Noto Color Emoji" ];
-    sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
-    serif = [ "Noto Serif" "Noto Color Emoji" ];
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "FiraCode Nerd Font" "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+      sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
+      serif     = [ "Noto Serif" "Noto Color Emoji" ];
+      emoji     = [ "Noto Color Emoji" ];
+    };
   };
 }
