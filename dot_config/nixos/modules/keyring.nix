346a9f54a1cb30e0ac13cd6d97329d6b1cdbaf32
Tommaso Soncin <soncintommaso@gmail.com>
2026-06-21 14:10:32 +0200

    chore: added autodecrypt

--- dot_config/nixos/modules/keyring.nix
+++ dot_config/nixos/modules/keyring.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
   environment.systemPackages = with pkgs; [ seahorse ];
 }
