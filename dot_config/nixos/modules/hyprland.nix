81f13bc49a0aa78c93fe1cf7c4a57d7b3b9232a2
Tommaso Soncin <soncintommaso@gmail.com>
2026-06-23 14:45:33 +0200

    fix: missing apps from niri

--- dot_config/nixos/modules/hyprland.nix
+++ dot_config/nixos/modules/hyprland.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
 
   services.displayManager.sddm = {
@@ -11,6 +11,17 @@
     enable = true;
     xwayland.enable = true;
   };
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+    config.common.default = [
+      "gnome"
+      "gtk"
+    ];
+  };
   security.polkit.enable = true;
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
@@ -18,6 +29,11 @@
     AQ_DRM_DEVICES = "/dev/dri/amd-igpu:/dev/dri/nvidia-dgpu";
   };
   environment.systemPackages = with pkgs; [
+    ghostty
+    fuzzel
+    inputs.noctalia.packages.${stdenv.hostPlatform.system}.default
+    playerctl
+    brightnessctl
     bibata-cursors
     grim
     slurp
