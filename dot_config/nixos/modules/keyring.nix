c37282443d96767dd2a5e129b570456145474260
Tommaso Soncin <soncintommaso@gmail.com>
2026-06-23 14:40:13 +0200

    attempting to fix keyring unlock

--- dot_config/nixos/modules/keyring.nix
+++ dot_config/nixos/modules/keyring.nix
@@ -3,4 +3,5 @@
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
   environment.systemPackages = with pkgs; [ seahorse ];
+  environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID"; # set the runtime directory
 }
